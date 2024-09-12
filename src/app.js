b86190028de5bed6dbed553479735b53ffa9409a
Tran Nguyen Xuan Khanh <trannguyenxuankhanh15@gmail.com>
2024-09-12 11:41:39 +0700

    Standardize JSON responses

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+
+const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
 
 const app = express();
@@ -7,11 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    return res.json({
-        message: 'ok'
-    });
+    return res.json(JSend.success());
 });
 contactsRouter.setup(app);
-
 module.exports = app;
